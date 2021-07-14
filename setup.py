06ed1ac01a8d2520df5a406afe13ded33407855a
Nathan Ramos, CFA® <86393277+nathanramoscfa@users.noreply.github.com>
2021-07-14 15:16:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='pyfinlab',  # Required
-    version='0.0.10',  # Required
+    version='0.0.11',  # Required
     description='Financial applications focusing on portfolio management',  # Optional
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)
